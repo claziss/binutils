37171247266988403723b04b34e0ce35a535ce61
Claudiu Zissulescu <claziss@synopsys.com>
2013-07-30 13:06:08 +0200

    Skeletron

--- gas/config/tc-arc.h
+++ gas/config/tc-arc.h
@@ -1,7 +1,6 @@
 /* tc-arc.h - Macros and type defines for the ARC.
    Copyright 1994, 1995, 1997, 2000, 2001, 2002, 2005, 2007
    Free Software Foundation, Inc.
-   Contributed by Doug Evans (dje@cygnus.com).
 
    This file is part of GAS, the GNU Assembler.
 
@@ -43,31 +42,18 @@
 
 #define BIG_ENDIAN      4321
 
-/* The endianness of the target format may change based on command
-   line arguments.  */
-extern const char * arc_target_format;
-
+#ifdef TARGET_BYTES_BIG_ENDIAN
+#define DEFAULT_TARGET_FORMAT  "elf32-bigarc"
+#define DEFAULT_BYTE_ORDER     BIG_ENDIAN
+#else
 #define DEFAULT_TARGET_FORMAT  "elf32-littlearc"
-#define TARGET_FORMAT          arc_target_format
 #define DEFAULT_BYTE_ORDER     LITTLE_ENDIAN
+#endif
+
+#define TARGET_FORMAT          DEFAULT_TARGET_FORMAT
 #define WORKING_DOT_WORD
 #define LISTING_HEADER         "ARC GAS "
 
-/* The ARC needs to parse reloc specifiers in .word.  */
-
-extern void arc_parse_cons_expression (struct expressionS *, unsigned);
-#define TC_PARSE_CONS_EXPRESSION(EXP, NBYTES) \
-  arc_parse_cons_expression (EXP, NBYTES)
-
-extern void arc_cons_fix_new (struct frag *, int, int, struct expressionS *);
-#define TC_CONS_FIX_NEW(FRAG, WHERE, NBYTES, EXP) \
-  arc_cons_fix_new (FRAG, WHERE, NBYTES, EXP)
-
-#define DWARF2_LINE_MIN_INSN_LENGTH 4
-
-/* Values passed to md_apply_fix don't include the symbol value.  */
-#define MD_APPLY_SYM_VALUE(FIX) 0
-
-/* No shared lib support, so we don't need to ensure externally
-   visible symbols can be overridden.  */
-#define EXTERN_FORCE_RELOC 0
+#ifndef TARGET_BYTES_BIG_ENDIAN
+#define TARGET_BYTES_BIG_ENDIAN 0
+#endif
