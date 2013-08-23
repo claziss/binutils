ced615965c0c5cf6186e17ab43554f2a9528af58
Claudiu Zissulescu <claziss@synopsys.com>
2013-08-23 16:25:06 +0200

    Elf changes

--- include/elf/common.h
+++ include/elf/common.h
@@ -193,7 +193,7 @@
 #define EM_MN10200	 90	/* Matsushita MN10200 */
 #define EM_PJ		 91	/* picoJava */
 #define EM_OPENRISC	 92	/* OpenRISC 32-bit embedded processor */
-#define EM_ARC_A5	 93	/* ARC Cores Tangent-A5 */
+#define EM_ARCOMPACT	 93	/* ARC-Compact Cores*/
 #define EM_XTENSA	 94	/* Tensilica Xtensa Architecture */
 #define EM_VIDEOCORE	 95	/* Alphamosaic VideoCore processor */
 #define EM_TMM_GPP	 96	/* Thompson Multimedia General Purpose Processor */
@@ -294,7 +294,8 @@
 #define EM_MICROBLAZE	189	/* Xilinx MicroBlaze 32-bit RISC soft processor core */
 #define EM_CUDA		190	/* NVIDIA CUDA architecture */
 #define EM_TILEGX	191	/* Tilera TILE-Gx multicore architecture family */
-#define EM_RL78		197	/* Renesas RL78 family.  */
+#define EM_ARCV2	195	/* ARC Compact V2 Cores */
+#define EM_RL78		197	/* Renesas RL78 family. */
 #define EM_78K0R	199	/* Renesas 78K0R.  */
 #define EM_INTEL205	205	/* Reserved by Intel */
 #define EM_INTEL206	206	/* Reserved by Intel */
